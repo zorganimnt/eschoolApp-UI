651fd9eb2d0439aedf0abebbf16b6bf6768cf68d
zorgani <zorgani.mnt@gmail.com>
2023-02-06 19:48:38 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/eschoolapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eschoolapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreeen extends StatelessWidget {
+  const HomeScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("repo jdid")),
+    );
+  }
+}
